9ef4c36cec191bbe9dd62191f6d68fbb9ee37238
Mamadou Billo Diallo <billodiallonet@gmail.com>
2020-05-10 11:19:31 +0300

    table users

--- app/models.py
+++ app/models.py
@@ -10,18 +10,8 @@ def load_user(user_id):
     return User.query.get(int(user_id))
 
 class User(UserMixin,db.Model):
-    __tablename__ = 'users'
-    id = db.Column(db.Integer,primary_key = True)
-    author = db.Column(db.String(255))
-    email = db.Column(db.String(255),unique = True,index = True)
-    password_hash = db.Column(db.String(255))
-    bio = db.Column(db.String(255))
-    profile_pic_path = db.Column(db.String())
-    blogpost = db.relationship('Blogs', backref='author', lazy='dynamic')
-    comments = db.relationship('Comments', backref='author', lazy='dynamic')
-    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
-    is_admin = db.Column(db.Boolean, default=False)
-
+    
+    
 #to link tables what you add after backref matters
     @property
     def password(self):
